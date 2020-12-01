6ccbff9c8c06f1f25607bce87f2589665fc04d8f
Jonas Maier <1.jmaier.3@gmail.com>
2020-12-01 17:17:41 +0100

    add embeds for random command

--- src/commands/util.rs
+++ src/commands/util.rs
@@ -1,21 +1,32 @@
 use crate::prelude::*;
+use rand::seq::SliceRandom;
 use serenity::framework::standard::{macros::command, CommandResult};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
+use serenity::utils::*;
 
 #[command]
 async fn random(ctx: &Context, msg: &Message) -> CommandResult {
     let mut args = msg.args();
-
     let mut words = Vec::new();
     while let Ok(word) = args.single::<String>() {
-        words.push(word);
+        words.push(word)
     }
 
-    if words.len() > 0 {
-        let word = &words[rand::random::<usize>() % words.len()];
-        msg.channel_id.say(&ctx.http, word).await?;
-    }
+    let word = words.choose(&mut rand::thread_rng());
+    msg.channel_id
+        .send_message(&ctx.http, |m| {
+            m.embed(|e| {
+                e.title("Random element");
+                if let Some(word) = word {
+                    e.description(format!("{} has chosen `{}` for you", NAME, word))
+                } else {
+                    e.colour(Colour::RED);
+                    e.description("Please specify a list of words.")
+                }
+            })
+        })
+        .await?;
 
     Ok(())
 }
